cd7c03cc37a4f45981cf820136699e1d08bb8a74
unknown <seletematome3@gmail.com>
2023-04-16 23:35:58 +0200

    title showing

--- src/TitleBar.java
+++ src/TitleBar.java
@@ -9,6 +9,9 @@ public class TitleBar extends JPanel {
         this.setPreferredSize(new Dimension(400, 80));
         this.setBackground(new Color(90, 143, 123));
 
-
+        TitleText.setPreferredSize(new Dimension(400, 80));
+        TitleText.setFont(new Font("Sans-sarif", Font.BOLD, 20));
+        TitleText.setHorizontalAlignment(JLabel.CENTER);
+        this.add(TitleText);
     }
 }
