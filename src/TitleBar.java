d5d2fb928530854e073be4724201e1df62ae05a9
unknown <seletematome3@gmail.com>
2023-04-16 23:05:06 +0200

    title bar and app frame displaying

---
+++ src/TitleBar.java
@@ -0,0 +1,14 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TitleBar extends JPanel {
+    public TitleBar() {
+        JLabel TitleText = new JLabel("To Do List App");
+
+        this.setPreferredSize(new Dimension(400, 80));
+        this.setBackground(new Color(90, 143, 123));
+
+
+    }
+}
---
+++ src/appFrame.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JFrame;
+public class appFrame extends JFrame {
+    public appFrame() {
+        this.setSize(400, 700);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+}
