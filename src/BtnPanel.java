5396b8dd8274e3b107ea2b2aff0dd704d22e7223
Matome <seletematome3@gmail.com>
2023-04-17 10:47:42 +0200

    clear and addtask are showing

---
+++ src/BtnPanel.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class BtnPanel extends JPanel {
+    private JButton addtask;
+    private JButton clear;
+
+    Border emptyborder = BorderFactory.createEmptyBorder();
+
+    public BtnPanel() {
+        this.setPreferredSize(new Dimension(400, 80));
+        this.setBackground(new Color(129, 202, 207));
+
+        addtask = new JButton("Add Task");
+        addtask.setBorder(emptyborder);
+        addtask.setFont(new Font("Sans-serif", Font.PLAIN, 20));
+        this.add(addtask);
+
+        this.add(Box.createHorizontalStrut(20));
+
+        clear = new JButton("Clear All Task");
+        clear.setBorder(emptyborder);
+        clear.setFont(new Font("Sans-serif", Font.PLAIN, 20));
+        this.add(clear);
+    }
+
+    public JButton getAddtaskbtn() {
+        return addtask;
+    }
+
+    public JButton getClearbtn() {
+        return clear;
+    }
+
+}
--- src/appFrame.java
+++ src/appFrame.java
@@ -1,15 +1,43 @@
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class appFrame extends JFrame {
 
+    private JButton  addtask;
+    private JButton clear;
+
     TitleBar title = new TitleBar();
+    BtnPanel btnPanel = new BtnPanel();
+
     public appFrame() {
         this.setSize(400, 700);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
         this.add(this.title, BorderLayout.NORTH);
+        this.add(btnPanel, BorderLayout.SOUTH);
+
+        addtask = btnPanel.getAddtaskbtn();
+        clear = btnPanel.getClearbtn();
+
+        addListener();
+    }
+
+    public void addListener() {
+
+        addtask.addMouseListener(new MouseAdapter() {
+            @Override
+
+            public void mousePressed(MouseEvent e) {
+
+
+
+            }
+
+        });
+
     }
 
 }
